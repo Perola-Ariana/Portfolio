2a49e8eea7fb5be6e1da0ded42241e40ef1bc4f7
Pérola Ariana <102548896+Perola-Ariana@users.noreply.github.com>
2023-02-10 01:51:47 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let menu = document.querySelector('.menu-icon')
+let navbar = document.querySelector(".navbar")
+menu.onclick = () => {
+    navbar.classList.toggle("open-menu")
+    menu.classList.toggle("move")
+
+}
+
+//header background change
+
+let header = document.querySelector('header')
+
+window.addEventListener('scroll', () =>{
+    header.classList.toggle('header-active', window.scrollY > 0)
+})
